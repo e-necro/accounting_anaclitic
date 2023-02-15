c939a190978d305835f42207b3dc1d13b45c5164
Necro The Human <necrothehuman@gmail.com>
2023-02-15 16:10:44 +0700

    auth, small refactoring

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,9 +5,15 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
-import mainStyles from '@/assets/css/main.css' ///TODO: помимо этих стилей, надо свои прикрутить, выяснив как с ними вообще работается. На тот же SASS
+import bootstrapStyles from '@/assets/css/bootstrap_imported.css' ///TODO: помимо этих стилей, надо свои прикрутить, выяснив как с ними вообще работается. На тот же SASS
+import mainStyles from '@/assets/css/main.css'
 
 
 
 
-createApp(App).use(store).use(router).use(mainStyles).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(bootstrapStyles)
+  .use(mainStyles)
+  .mount('#app')
