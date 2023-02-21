42aea16be91d5b1b27618477e622a8c4df18f4e5
necro <necrothehuman@gmail.com>
2023-02-21 22:33:08 +0700

    auth, remove bootstrap from npm

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,6 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
-import bootstrapStyles from '@/assets/css/bootstrap_imported.css' ///TODO: помимо этих стилей, надо свои прикрутить, выяснив как с ними вообще работается. На тот же SASS
 import mainStyles from '@/assets/css/main.css'
 
 
@@ -14,6 +13,5 @@ import mainStyles from '@/assets/css/main.css'
 createApp(App)
   .use(store)
   .use(router)
-  .use(bootstrapStyles)
   .use(mainStyles)
   .mount('#app')
